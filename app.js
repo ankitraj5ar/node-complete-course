526eb7400413393138227db49ac1a1111efdce28
Ankit Raj <ankitraj5ar@gmail.com>
2024-11-04 09:18:27 +0530

    fresh start with mongodb

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
-import sequelize from "./util/database.js";
+import mongodb from "./util/database.js";
 
 import errorController from "./controllers/error.js";
-import adminRoutes from "./routes/admin.js"; // Add .js extension for ES module imports
-import shopRoutes from "./routes/shop.js"; // Add .js extension for ES module imports
-import Product from "./models/product.js";
-import User from "./models/user.js";
-import Cart from "./models/cart.js";
-import CartDetail from "./models/cartDetail.js";
-import Order from "./models/order.js";
-import OrderDetail from "./models/orderDetail.js";
+// import adminRoutes from "./routes/admin.js"; // Add .js extension for ES module imports
+// import shopRoutes from "./routes/shop.js"; // Add .js extension for ES module imports
+// import Product from "./models/product.js";
+// import User from "./models/user.js";
+// import Cart from "./models/cart.js";
+// import CartDetail from "./models/cartDetail.js";
+// import Order from "./models/order.js";
+// import OrderDetail from "./models/orderDetail.js";
 // Get the current file's directory
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -20,47 +20,19 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(async (req, res, next) => {
-  const user = await User.findByPk(1);
-  req.user = user;
-  next();
-});
+// app.use(async (req, res, next) => {
+//   const user = await User.findByPk(1);
+//   req.user = user;
+//   next();
+// });
 app.set("view engine", "ejs");
 app.set("views", "views");
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/admin", adminRoutes);
-app.use(shopRoutes);
+// app.use("/admin", adminRoutes);
+// app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-// associations
-Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
-User.hasMany(Product);
-
-User.hasOne(Cart);
-Cart.belongsTo(User);
-
-Cart.belongsToMany(Product, { through: CartDetail });
-Product.belongsToMany(Cart, { through: CartDetail });
-
-Order.belongsTo(User);
-User.hasMany(Order);
-Order.belongsToMany(Product, { through: OrderDetail });
-
-// await sequelize.sync({ force: true });
-await sequelize.sync();
-const user = await User.findByPk(1);
-if (!user) {
-  await User.create({
-    txt_name: "Ankit Raj",
-    txt_email: "ankitraj5ar@gmail.com",
-  });
-}
-const cart = await Cart.findByPk(1);
-if (!cart) {
-  await user.createCart();
-}
-
-// console.log(result);
+console.log(mongodb);
 app.listen(3000);
--- util/database.js
+++ util/database.js
@@ -1,7 +1,32 @@
-import { Sequelize } from "sequelize";
-const sequelize = new Sequelize("node_complete", "root", "Admin@123", {
-  dialect: "mysql",
-  host: "localhost",
-});
+// import { Sequelize } from "sequelize";
+// const sequelize = new Sequelize("node_complete", "root", "Admin@123", {
+//   dialect: "mysql",
+//   host: "localhost",
+// });
 
-export default sequelize;
+// export default sequelize;
+
+/// mongodb connection
+
+import { MongoClient } from "mongodb";
+
+const url = "mongodb://127.0.0.1:27017"; // Local MongoDB URL
+const dbName = "nodejscomplete"; // Replace with your database name
+
+async function connectToMongo() {
+  const client = new MongoClient(url);
+
+  try {
+    await client.connect();
+    console.log("Connected successfully to MongoDB");
+
+    return client.db(dbName);
+    // Perform your database operations here
+  } catch (error) {
+    console.error("Connection failed", error);
+  } finally {
+    await client.close(); // Ensure the client is closed after operations
+  }
+}
+
+export default await connectToMongo();
